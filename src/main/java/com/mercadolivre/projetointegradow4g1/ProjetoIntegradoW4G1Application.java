ea39899f254b180f55029889967c172934083fdb
AlexandreLaurenti <alexandre.dumas@mercadolivre.com>
2022-01-26 18:44:55 -0300

    Commit inicial

---
+++ src/main/java/com/mercadolivre/projetointegradow4g1/ProjetoIntegradoW4G1Application.java
@@ -0,0 +1,13 @@
+package com.mercadolivre.projetointegradow4g1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoIntegradoW4G1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoIntegradoW4G1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/mercadolivre/projetointegradow4g1/ProjetoIntegradoW4G1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercadolivre.projetointegradow4g1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoIntegradoW4G1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
