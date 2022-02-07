8a47b3bd8ea8001761443d79c7b224b50d11df5e
jefamorim <jeferson.amorim@mercadolivre.com>
2022-02-07 16:33:16 -0300

    Testes unitários

--- src/test/java/com/mercadolivre/projetointegradow4g1/services/AnuncioServiceTest.java
+++ src/test/java/com/mercadolivre/projetointegradow4g1/services/AnuncioServiceTest.java
@@ -1,7 +1,38 @@
 package com.mercadolivre.projetointegradow4g1.services;
 
+import com.mercadolivre.projetointegradow4g1.entities.Anuncio;
+import com.mercadolivre.projetointegradow4g1.entities.Lote;
+import com.mercadolivre.projetointegradow4g1.entities.Vendedor;
+import com.mercadolivre.projetointegradow4g1.repositories.AnuncioRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class AnuncioServiceTest {
 
+    @Test
+    public void deveCadastrarAnuncio(){
+        AnuncioRepository mock = Mockito.mock(AnuncioRepository.class);
+
+        Anuncio anuncio = Anuncio.builder()
+                .id(1L)
+                .titulo("Anuncio 1")
+                .descricao("Descrição anuncio 1")
+                .preco(BigDecimal.valueOf(100.00))
+                .lote(Lote.builder().build())
+                .vendedor(Vendedor.builder().build())
+                .build();
+
+        Mockito.when(mock.save(Mockito.any(Anuncio.class))).thenReturn(anuncio);
+
+        AnuncioService anuncioService = new AnuncioService(mock);
+        anuncioService.salvarAnuncio(anuncio);
+        assertNotNull(anuncio.getId());
+
+    }
 }
 
 
--- src/test/java/com/mercadolivre/projetointegradow4g1/services/RegistroDeEstoqueServiceTest.java
+++ src/test/java/com/mercadolivre/projetointegradow4g1/services/RegistroDeEstoqueServiceTest.java
@@ -1,5 +1,37 @@
+/*
 package com.mercadolivre.projetointegradow4g1.services;
+
+import com.mercadolivre.projetointegradow4g1.entities.Lote;
+import com.mercadolivre.projetointegradow4g1.entities.RegistroDeEstoque;
+import com.mercadolivre.projetointegradow4g1.entities.Representante;
+import com.mercadolivre.projetointegradow4g1.entities.Setor;
+import com.mercadolivre.projetointegradow4g1.repositories.RegistroDeEstoqueRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.time.Instant;
+import java.util.Set;
 
 public class RegistroDeEstoqueServiceTest {
 
-}
+    @Test
+    public void deveCadastrarRegistroDeEstoque(){
+        RegistroDeEstoqueRepository mock = Mockito.mock(RegistroDeEstoqueRepository.class);
+
+        RegistroDeEstoque registroDeEstoque = RegistroDeEstoque.builder()
+                .id(1L)
+                .data(Instant.parse("2018-05-07T15:20:45.765Z"))
+                .setor(Setor.builder().build())
+                .lotes()
+                .representante(Representante.builder().build())
+                .build();
+
+        Mockito.when(mock.save(Mockito.any(RegistroDeEstoque.class))).thenReturn(registroDeEstoque);
+
+        RegistroDeEstoqueService registroDeEstoqueService = new RegistroDeEstoqueService(mock);
+        registroDeEstoqueService.salvarRegistroDeEstoque(registroDeEstoque);
+        assertNotNull(registroDeEstoque.getId());
+
+
+    }
+}*/
