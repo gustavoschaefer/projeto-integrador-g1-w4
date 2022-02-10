2da7e81b38e7c29bfa9e7432d4f5b18062cfbc8b
apraca <0906Rej@ne87>
2022-02-10 14:28:11 -0300

    Calcula Frete test

--- src/test/java/com/mercadolivre/projetointegradow4g1/services/EstadoDeDestinoServiceTest.java
+++ src/test/java/com/mercadolivre/projetointegradow4g1/services/EstadoDeDestinoServiceTest.java
@@ -3,8 +3,10 @@ package com.mercadolivre.projetointegradow4g1.services;
 import com.mercadolivre.projetointegradow4g1.entities.*;
 import com.mercadolivre.projetointegradow4g1.repositories.EstadoDestinoRepository;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -63,4 +65,191 @@ public class EstadoDeDestinoServiceTest {
         assertEquals("Rio de Janeiro", lista.get(1).getNome());
     }
 
+    @Test
+    public void deveCalcularFreteSP() {
+        EstadoDestinoRepository mock = Mockito.mock(EstadoDestinoRepository.class);
+            new EstadoDestinoService(mock);
+            assertEquals(EstadoDestinoService.calculaFrete("SP",16), BigDecimal.valueOf(480.0));
+    }
+
+    @Test
+    public void deveCalcularFreteRJ() {
+        EstadoDestinoRepository mock = Mockito.mock(EstadoDestinoRepository.class);
+        new EstadoDestinoService(mock);
+        assertEquals(EstadoDestinoService.calculaFrete("RJ",10), BigDecimal.valueOf(180.0));
+    }
+
+    @Test
+    public void deveCalcularFreteMG() {
+        EstadoDestinoRepository mock = Mockito.mock(EstadoDestinoRepository.class);
+        new EstadoDestinoService(mock);
+        assertEquals(EstadoDestinoService.calculaFrete("MG",10), BigDecimal.valueOf(190.0));
+    }
+
+    @Test
+    public void deveCalcularFreteES() {
+        EstadoDestinoRepository mock = Mockito.mock(EstadoDestinoRepository.class);
+        new EstadoDestinoService(mock);
+        assertEquals(EstadoDestinoService.calculaFrete("ES",10), BigDecimal.valueOf(190.0));
+    }
+
+    @Test
+    public void deveCalcularFreteMT() {
+        EstadoDestinoRepository mock = Mockito.mock(EstadoDestinoRepository.class);
+        new EstadoDestinoService(mock);
+        assertEquals(EstadoDestinoService.calculaFrete("MT",10), BigDecimal.valueOf(200.0));
+    }
+
+    @Test
+    public void deveCalcularFreteMS() {
+        EstadoDestinoRepository mock = Mockito.mock(EstadoDestinoRepository.class);
+        new EstadoDestinoService(mock);
+        assertEquals(EstadoDestinoService.calculaFrete("MS",10), BigDecimal.valueOf(200.0));
+    }
+
+    @Test
+    public void deveCalcularFreteRS() {
+        EstadoDestinoRepository mock = Mockito.mock(EstadoDestinoRepository.class);
+        new EstadoDestinoService(mock);
+        assertEquals(EstadoDestinoService.calculaFrete("RS",10), BigDecimal.valueOf(250.0));
+    }
+
+    @Test
+    public void deveCalcularFretePR() {
+        EstadoDestinoRepository mock = Mockito.mock(EstadoDestinoRepository.class);
+        new EstadoDestinoService(mock);
+        assertEquals(EstadoDestinoService.calculaFrete("PR",10), BigDecimal.valueOf(180.0));
+    }
+
+    @Test
+    public void deveCalcularFreteSC() {
+        EstadoDestinoRepository mock = Mockito.mock(EstadoDestinoRepository.class);
+        new EstadoDestinoService(mock);
+        assertEquals(EstadoDestinoService.calculaFrete("SC",10), BigDecimal.valueOf(200.0));
+    }
+
+    @Test
+    public void deveCalcularFretePB() {
+        EstadoDestinoRepository mock = Mockito.mock(EstadoDestinoRepository.class);
+        new EstadoDestinoService(mock);
+        assertEquals(EstadoDestinoService.calculaFrete("PB",10), BigDecimal.valueOf(220.0));
+    }
+
+    @Test
+    public void deveCalcularFreteSE() {
+        EstadoDestinoRepository mock = Mockito.mock(EstadoDestinoRepository.class);
+        new EstadoDestinoService(mock);
+        assertEquals(EstadoDestinoService.calculaFrete("SE",10), BigDecimal.valueOf(200.0));
+    }
+    @Test
+    public void deveCalcularFreteAL() {
+        EstadoDestinoRepository mock = Mockito.mock(EstadoDestinoRepository.class);
+        new EstadoDestinoService(mock);
+        assertEquals(EstadoDestinoService.calculaFrete("AL",10), BigDecimal.valueOf(200.0));
+    }
+
+    @Test
+    public void deveCalcularFreteCE() {
+        EstadoDestinoRepository mock = Mockito.mock(EstadoDestinoRepository.class);
+        new EstadoDestinoService(mock);
+        assertEquals(EstadoDestinoService.calculaFrete("CE",10), BigDecimal.valueOf(210.0));
+    }
+
+    @Test
+    public void deveCalcularFretePE() {
+        EstadoDestinoRepository mock = Mockito.mock(EstadoDestinoRepository.class);
+        new EstadoDestinoService(mock);
+        assertEquals(EstadoDestinoService.calculaFrete("PE",10), BigDecimal.valueOf(250.0));
+    }
+
+    @Test
+    public void deveCalcularFretePI() {
+        EstadoDestinoRepository mock = Mockito.mock(EstadoDestinoRepository.class);
+        new EstadoDestinoService(mock);
+        assertEquals(EstadoDestinoService.calculaFrete("PI",10), BigDecimal.valueOf(250.0));
+    }
+
+    @Test
+    public void deveCalcularFretePA() {
+        EstadoDestinoRepository mock = Mockito.mock(EstadoDestinoRepository.class);
+        new EstadoDestinoService(mock);
+        assertEquals(EstadoDestinoService.calculaFrete("PA",10), BigDecimal.valueOf(200.0));
+    }
+
+    @Test
+    public void deveCalcularFreteAM() {
+        EstadoDestinoRepository mock = Mockito.mock(EstadoDestinoRepository.class);
+        new EstadoDestinoService(mock);
+        assertEquals(EstadoDestinoService.calculaFrete("AM",10), BigDecimal.valueOf(300.0));
+    }
+
+    @Test
+    public void deveCalcularFreteAP() {
+        EstadoDestinoRepository mock = Mockito.mock(EstadoDestinoRepository.class);
+        new EstadoDestinoService(mock);
+        assertEquals(EstadoDestinoService.calculaFrete("AP",10), BigDecimal.valueOf(300.0));
+    }
+
+    @Test
+    public void deveCalcularFreteRO() {
+        EstadoDestinoRepository mock = Mockito.mock(EstadoDestinoRepository.class);
+        new EstadoDestinoService(mock);
+        assertEquals(EstadoDestinoService.calculaFrete("RO",10), BigDecimal.valueOf(320.0));
+    }
+
+    @Test
+    public void deveCalcularFreteRM() {
+        EstadoDestinoRepository mock = Mockito.mock(EstadoDestinoRepository.class);
+        new EstadoDestinoService(mock);
+        assertEquals(EstadoDestinoService.calculaFrete("RM",10), BigDecimal.valueOf(300.0));
+    }
+
+    @Test
+    public void deveCalcularFreteGO() {
+        EstadoDestinoRepository mock = Mockito.mock(EstadoDestinoRepository.class);
+        new EstadoDestinoService(mock);
+        assertEquals(EstadoDestinoService.calculaFrete("GO",10), BigDecimal.valueOf(200.0));
+    }
+
+    @Test
+    public void deveCalcularFreteDF() {
+        EstadoDestinoRepository mock = Mockito.mock(EstadoDestinoRepository.class);
+        new EstadoDestinoService(mock);
+        assertEquals(EstadoDestinoService.calculaFrete("DF",10), BigDecimal.valueOf(200.0));
+    }
+
+    @Test
+    public void deveCalcularFreteRN() {
+        EstadoDestinoRepository mock = Mockito.mock(EstadoDestinoRepository.class);
+        new EstadoDestinoService(mock);
+        assertEquals(EstadoDestinoService.calculaFrete("RN",10), BigDecimal.valueOf(260.0));
+    }
+
+    @Test
+    public void deveCalcularFreteBA() {
+        EstadoDestinoRepository mock = Mockito.mock(EstadoDestinoRepository.class);
+        new EstadoDestinoService(mock);
+        assertEquals(EstadoDestinoService.calculaFrete("BA",10), BigDecimal.valueOf(200.0));
+    }
+
+    @Test
+    public void deveCalcularFreteMA() {
+        EstadoDestinoRepository mock = Mockito.mock(EstadoDestinoRepository.class);
+        new EstadoDestinoService(mock);
+        assertEquals(EstadoDestinoService.calculaFrete("MA",10), BigDecimal.valueOf(220.0));
+    }
+
+    @Test
+    public void deveCalcularFreteAC() {
+        EstadoDestinoRepository mock = Mockito.mock(EstadoDestinoRepository.class);
+        new EstadoDestinoService(mock);
+        assertEquals(EstadoDestinoService.calculaFrete("AC",10), BigDecimal.valueOf(300.0));
+    }
+
+    @Test
+    public void deveCalcularFreteTO() {
+        EstadoDestinoRepository mock = Mockito.mock(EstadoDestinoRepository.class);
+        new EstadoDestinoService(mock);
+        assertEquals(EstadoDestinoService.calculaFrete("TO",10), BigDecimal.valueOf(220.0));
+    }
 }
